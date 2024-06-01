850e9e9368260aafaa6a780cdabe6637a3a1aee9
nageshgitversion <rasukuntanagesh@gmail.com>
2024-06-01 12:07:56 +0530

    FIRST EPISODE

---
+++ App.js
@@ -0,0 +1,17 @@
+// const heading1 = document.createElement("h1")
+// heading.innerHTML = "Hello I am new to Java Script step into first stage 🚀"
+// const root =  document.getElementById("root")
+// root.appendChild(heading1)
+
+
+// const heading = React.createElement("h1" ,{},"Hello i am new to React");
+
+
+
+const heading = <h1>"Hello everybody"</h1>
+ 
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(<heading/>)
+
+
